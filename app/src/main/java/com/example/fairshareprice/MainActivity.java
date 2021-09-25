8eae9195284aec6a91ff88d9bfb34441b5a88f01
AndreyTrain <andrey_training@bk.ru>
2021-09-25 23:58:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fairshareprice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fairshareprice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
